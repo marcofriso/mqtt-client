6adb1a1c196f2deee70cdd88ce73bfd04e3ee805
Marco F <marco.friso@gmail.com>
2023-02-11 18:54:52 +0100

    fix: change Connect component, add eslint-visitor-keys dependency

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,34 +1,54 @@
-import React from 'react';
-import { Card, Button, Form, Input, Row, Col } from 'antd';
+import React, { useState, useEffect } from "react";
+import { Card, Button, Form, Input, Row, Col } from "antd";
 
 const Connection = ({ connect, disconnect, connectBtn }) => {
   const [form] = Form.useForm();
+  const [detailsDisplayProperty, setDetailsDisplayProperty] = useState("none");
+  const [formFieldsDisabled, setFormFieldsDisabled] = useState("");
+
   const record = {
-    host: 'broker.emqx.io',
+    host: "broker.emqx.io",
     clientId: `mqttjs_ + ${Math.random().toString(16).substr(2, 8)}`,
     port: 8083,
   };
+
+  const options = {
+    keepalive: 30,
+    protocolId: "MQTT",
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    will: {
+      topic: "WillMsg",
+      payload: "Connection Closed abnormally..!",
+      qos: 0,
+      retain: false,
+    },
+    rejectUnauthorized: false,
+  };
+
+  const buttonsStyle = {
+    marginTop: "5px",
+    marginLeft: "10px",
+    display: "inline-flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  useEffect(() => {
+    connectBtn === "Connected"
+      ? setFormFieldsDisabled("disabled")
+      : setFormFieldsDisabled("");
+  }, [connectBtn]);
+
   const onFinish = (values) => {
     const { host, clientId, port, username, password } = values;
     const url = `ws://${host}:${port}/mqtt`;
-    const options = {
-      keepalive: 30,
-      protocolId: 'MQTT',
-      protocolVersion: 4,
-      clean: true,
-      reconnectPeriod: 1000,
-      connectTimeout: 30 * 1000,
-      will: {
-        topic: 'WillMsg',
-        payload: 'Connection Closed abnormally..!',
-        qos: 0,
-        retain: false
-      },
-      rejectUnauthorized: false
-    };
     options.clientId = clientId;
     options.username = username;
     options.password = password;
+
     connect(url, options);
   };
 
@@ -40,70 +60,79 @@ const Connection = ({ connect, disconnect, connectBtn }) => {
     disconnect();
   };
 
-  const ConnectionForm = (
-    <Form
-      layout="vertical"
-      name="basic"
-      form={form}
-      initialValues={record}
-      onFinish={onFinish}
-    >
-      <Row gutter={20}>
-        <Col span={8}>
-          <Form.Item
-            label="Host"
-            name="host"
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item
-            label="Port"
-            name="port"
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item
-            label="Client ID"
-            name="clientId"
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item
-            label="Username"
-            name="username"
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item
-            label="Password"
-            name="password"
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-      </Row>
-    </Form>
-  )
+  const toggleDetailsDisplayProperty = () => {
+    detailsDisplayProperty === "none"
+      ? setDetailsDisplayProperty("block")
+      : setDetailsDisplayProperty("none");
+  };
+
+  const Details = (
+    <Row style={{ display: detailsDisplayProperty }}>
+      <Col span={24}>
+        <h6 style={{ width: "100%", textAlign: "center" }}>Details</h6>
+        <Row gutter={20}>
+          <Col span={12}>
+            <Form.Item label="Host" name="host">
+              <Input disabled={formFieldsDisabled} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Port" name="port">
+              <Input disabled={formFieldsDisabled} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Client ID" name="clientId">
+              <Input disabled={formFieldsDisabled} />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 
   return (
-    <Card
-      title="Connection"
-      actions={[
-        <Button type="primary" onClick={handleConnect}>{connectBtn}</Button>,
-        <Button danger onClick={handleDisconnect}>Disconnect</Button>
-      ]}
-    >
-      {ConnectionForm}
+    <Card title={`Connection: ${connectBtn}`}>
+      <Form
+        layout="vertical"
+        name="basic"
+        form={form}
+        initialValues={record}
+        onFinish={onFinish}
+      >
+        <Row>
+          <Col span={11}>
+            <Form.Item label="Username" name="username">
+              <Input disabled={formFieldsDisabled} />
+            </Form.Item>
+          </Col>
+          <Col span={6} style={buttonsStyle}>
+            {connectBtn === "Connected" ? (
+              <Button block danger onClick={handleDisconnect}>
+                Disconnect
+              </Button>
+            ) : (
+              <Button block type="primary" onClick={handleConnect}>
+                Connect
+              </Button>
+            )}
+          </Col>
+          <Col span={6} style={buttonsStyle}>
+            <Button
+              block
+              type="secondary"
+              onClick={() => {
+                toggleDetailsDisplayProperty();
+              }}
+            >
+              Modify
+            </Button>
+          </Col>
+        </Row>
+        {Details}
+      </Form>
     </Card>
   );
-}
+};
 
 export default Connection;
--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,22 @@
-import React, { createContext, useEffect, useState } from 'react';
-import Connection from './Connection';
-import Publisher from './Publisher';
-import Subscriber from './Subscriber';
-import Receiver from './Receiver';
-import mqtt from 'mqtt';
+import React, { createContext, useEffect, useState } from "react";
+import Connection from "./Connection";
+import Publisher from "./Publisher";
+import Subscriber from "./Subscriber";
+import Receiver from "./Receiver";
+import mqtt from "mqtt";
 
-export const QosOption = createContext([])
+export const QosOption = createContext([]);
 const qosOption = [
   {
-    label: '0',
+    label: "0",
     value: 0,
-  }, {
-    label: '1',
+  },
+  {
+    label: "1",
     value: 1,
-  }, {
-    label: '2',
+  },
+  {
+    label: "2",
     value: 2,
   },
 ];
@@ -23,27 +25,33 @@ const HookMqtt = () => {
   const [client, setClient] = useState(null);
   const [isSubed, setIsSub] = useState(false);
   const [payload, setPayload] = useState({});
-  const [connectStatus, setConnectStatus] = useState('Connect');
+  const [connectStatus, setConnectStatus] = useState("Disconnected");
 
   const mqttConnect = (host, mqttOption) => {
-    setConnectStatus('Connecting');
+    setConnectStatus("Connecting");
     setClient(mqtt.connect(host, mqttOption));
   };
 
   useEffect(() => {
     if (client) {
-      client.on('connect', () => {
-        setConnectStatus('Connected');
+      client.on("connect", () => {
+        setConnectStatus("Connected");
       });
-      client.on('error', (err) => {
-        console.error('Connection error: ', err);
+      client.on("error", (err) => {
+        console.error("Connection error: ", err);
         client.end();
       });
-      client.on('reconnect', () => {
-        setConnectStatus('Reconnecting');
+      client.on("reconnect", () => {
+        setConnectStatus("Reconnecting");
       });
-      client.on('message', (topic, message) => {
+      client.on("message", (topic, message, packet) => {
         const payload = { topic, message: message.toString() };
+        console.log(
+          "RUNS",
+          JSON.parse(message.toString()),
+          "\nMessage",
+          message.toString()
+        );
         setPayload(payload);
       });
     }
@@ -52,31 +60,37 @@ const HookMqtt = () => {
   const mqttDisconnect = () => {
     if (client) {
       client.end(() => {
-        setConnectStatus('Connect');
+        setConnectStatus("Disconnected");
       });
     }
-  }
+  };
 
   const mqttPublish = (context) => {
     if (client) {
       const { topic, qos, payload } = context;
-      client.publish(topic, payload, { qos }, error => {
+      const payload2 = JSON.stringify({ message: payload, userId: "XXXY" });
+      client.publish(topic, payload2, { qos }, (error) => {
         if (error) {
-          console.log('Publish error: ', error);
+          console.log("Publish error: ", error);
         }
       });
     }
-  }
+  };
 
   const mqttSub = (subscription) => {
     if (client) {
       const { topic, qos } = subscription;
       client.subscribe(topic, { qos }, (error) => {
         if (error) {
-          console.log('Subscribe to topics error', error)
-          return
+          console.log("Subscribe to topics error", error);
+          return;
         }
-        setIsSub(true)
+        console.log("SUBSCRIBED", subscription);
+        setIsSub(true);
+
+        if (!error) {
+          client.publish("testtopic/react", JSON.stringify(`Test MODICA`));
+        }
       });
     }
   };
@@ -84,11 +98,12 @@ const HookMqtt = () => {
   const mqttUnSub = (subscription) => {
     if (client) {
       const { topic } = subscription;
-      client.unsubscribe(topic, error => {
+      client.unsubscribe(topic, (error) => {
         if (error) {
-          console.log('Unsubscribe error', error)
-          return
+          console.log("Unsubscribe error", error);
+          return;
         }
+
         setIsSub(false);
       });
     }
@@ -96,14 +111,18 @@ const HookMqtt = () => {
 
   return (
     <>
-      <Connection connect={mqttConnect} disconnect={mqttDisconnect} connectBtn={connectStatus} />
+      <Connection
+        connect={mqttConnect}
+        disconnect={mqttDisconnect}
+        connectBtn={connectStatus}
+      />
       <QosOption.Provider value={qosOption}>
         <Subscriber sub={mqttSub} unSub={mqttUnSub} showUnsub={isSubed} />
         <Publisher publish={mqttPublish} />
       </QosOption.Provider>
-      <Receiver payload={payload}/>
+      <Receiver payload={payload} />
     </>
   );
-}
+};
 
 export default HookMqtt;
